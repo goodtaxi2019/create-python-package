7c231d7ff1add4a966336ea6843d800717b212c1
dceoy <dnarsil+github@gmail.com>
2019-12-19 01:56:26 +0900

    Add templates

--- pypkgcreator/cli/main.py
+++ pypkgcreator/cli/main.py
@@ -5,14 +5,12 @@ Python package scaffold builder
 Usage:
     create-python-package -h|--help
     create-python-package -v|--version
-    create-python-package [--debug|--info] [--docker] [--module=<name>] <path>
+    create-python-package [--debug|--info] [--module=<name>] <path>
 
 Options:
     -h, --help          Print help and exit
     -v, --version       Print version and exit
     --debug, --info     Execute a command with debug|info messages
-    --docker            Create Dockerfile and docker-compose.yml
-    --static            Include static fileas
     --module=<name>     Specify a module name
 
 Arguments:
@@ -25,6 +23,7 @@ import re
 from pathlib import PurePath
 
 from docopt import docopt
+from jinja2 import Environment, FileSystemLoader
 
 from .. import __version__
 
@@ -36,7 +35,20 @@ def main():
     _set_log_config(debug=args['--debug'], info=args['--info'])
     logger = logging.getLogger(__name__)
     logger.debug('args:{0}{1}'.format(os.linesep, args))
-    create_python_package_scaffold(args=args)
+    _create_python_package_scaffold(args=args)
+
+
+def _create_python_package_scaffold(args, include_package_data=True,
+                                    create_dockerfile=True):
+    project_path = PurePath(args['<path>'])
+    package_name = args['--module'] or project_path.name
+    package_path = project_path.joinpath(re.sub(r'[\.\-]', '_', package_name))
+    package_path.mkdir(exist_ok=True)
+    data = {
+        'package_name': package_name, 'module_name': package_path.name,
+        'include_package_data': str(include_package_data),
+        'version': 'v0.0.1', 'description': '', 'user_name': '', 'url': ''
+    }
 
 
 def _set_log_config(debug=None, info=None):
@@ -52,8 +64,13 @@ def _set_log_config(debug=None, info=None):
     )
 
 
-def create_python_package_scaffold(args):
-    project_path = PurePath(args['<path>'])
-    module_name = re.sub(
-        r'[\.\-]', '_', (args['--module'] or project_path.name)
-    )
+def _render_j2_template(data, j2_template, output_path):
+    with open(output_path, 'w') as f:
+        f.write(
+            Environment(
+                loader=FileSystemLoader(
+                    PurePath(__file__).parents[1].joinpath('template'),
+                    encoding='utf8'
+                )
+            ).get_template(j2_template).render(data).encode('utf-8')
+        )
